1411c86ea4a0fede479587ca1d2c58010e0e99c9
Mitsunorih <Mitsunorihk@gmail.com>
2025-07-30 10:22:45 -0600

    pid hecho 30jul 1022am

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/SampleMechanism/ArmMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/SampleMechanism/ArmMotor.java
@@ -1,29 +1,38 @@
 package org.firstinspires.ftc.teamcode.Mechanisms.SampleMechanism;
 
+import com.arcrobotics.ftclib.controller.PIDController;
+
 import org.firstinspires.ftc.teamcode.Core.BaseClasses.EctoMechanism;
 import org.firstinspires.ftc.teamcode.Core.Control.EctoMotor.EctoMotor;
 import org.firstinspires.ftc.teamcode.Core.Control.EctoMotor.EctoMotorConfig;
+import org.firstinspires.ftc.teamcode.Core.Utils.EctoMath;
 
-public class RollerMotor extends EctoMechanism {
-    EctoMotor rollerMotor;
+public class ArmMotor extends EctoMechanism {
+    EctoMotor armMotor;
+
+    PIDController pidMotor;
     EctoMotorConfig motorrConfig;
 
-    public RollerMotor(String moduleName, String moduleType) {
+    public ArmMotor(String moduleName, String moduleType) {
         super(moduleName, moduleType);
     }
 
-    public void set(double percentage) {
-        rollerMotor.set(percentage);
+    public void set(double degrees) {
+        pidMotor.setSetPoint(EctoMath.degToRad(degrees));
+
     }
 
 
     @Override
     public void initMechanism() {
         motorrConfig = new EctoMotorConfig();
-        motorrConfig.withId("RollerMotor");
+        motorrConfig.withId("ArmMotor");
         motorrConfig.withEnableForwardSoftLimit(false);
         motorrConfig.withEnableReverseSoftLimit(false);
-        rollerMotor = new EctoMotor(hardwareMap,motorrConfig);
+        motorrConfig.withTicksPerRev(4);
+        motorrConfig.withInternalMotorGearRatio(72/1);
+        armMotor = new EctoMotor(hardwareMap,motorrConfig);
+        pidMotor = new PIDController(0.1,0,0);
 
     }
 
@@ -34,7 +43,11 @@ public class RollerMotor extends EctoMechanism {
 
     @Override
     public void updateMechanism() {
-        rollerMotor.update();
+        armMotor.update();
+        double out;
+        out = pidMotor.calculate(armMotor.getPose_rad());
+        armMotor.set(out);
+
 
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotOpModes/TeleOperated/ArmMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotOpModes/TeleOperated/ArmMotorTest.java
@@ -1,20 +1,19 @@
 package org.firstinspires.ftc.teamcode.RobotOpModes.TeleOperated;
 
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.Core.BaseClasses.OperationModes.EctoOpMode;
-import org.firstinspires.ftc.teamcode.Mechanisms.SampleMechanism.RollerMotor;
+import org.firstinspires.ftc.teamcode.Mechanisms.SampleMechanism.ArmMotor;
 
-public class RollerMotorTest extends EctoOpMode {
+public class ArmMotorTest extends EctoOpMode {
 
-    RollerMotor rollerMotor;
+    ArmMotor armMotor;
 
     GamepadEx gamepad;
     @Override
     public void initRobotClasses() {
-        rollerMotor = new RollerMotor("RollerMotor", "Mechanism");
-        mechanismManager.addMechanism(rollerMotor);
+        armMotor = new ArmMotor("RollerMotor", "Mechanism");
+        mechanismManager.addMechanism(armMotor);
 
         //control
         gamepad = new GamepadEx(gamepad1);
@@ -34,11 +33,11 @@ public class RollerMotorTest extends EctoOpMode {
     public void updateRobot(Double timeStep) {
 
         if (gamepad.getButton(RobotConfiguration.Buttons.rightBumper)) {
-            rollerMotor.set(30);
+            armMotor.set(30);
         }else if (gamepad.getButton(RobotConfiguration.Buttons.leftBumper)) {
-            rollerMotor.set(-30);
+            armMotor.set(-30);
         }else {
-            rollerMotor.set(0);
+            armMotor.set(0);
             }
 
 
